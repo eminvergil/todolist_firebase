fa4b330590f8e79f5833643c7fabcfa36c99f8dd
eminvergil <emintrhack@gmail.com>
2020-08-18 19:00:04 +0300

    firebase database,necessary changes in app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,55 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Todo List App Made With Firebase</h1>
-      <input placeholder="Add your todo item"></input>
-      <button>Add TODO</button>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newNote: "",
+      noteList: [],
+      idList: [33, 55],
+    };
+  }
+
+  updateInput(key, value) {
+    this.setState({
+      [key]: value,
+    });
+  }
+
+  addItem() {
+    const newNote = {
+      id: 1 + Math.random(),
+      value: this.state.newNote.slice(),
+    };
+
+    const noteList = [...this.state.noteList];
+
+    noteList.push(newNote);
+
+    this.setState({
+      newNote: "",
+      noteList,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Todo List App Made With Firebase</h1>
+        <input
+          type="text"
+          placeholder="Add your todo item"
+          value={this.state.newNote}
+          onChange={(e) => this.updateInput("newNote", e.target.value)}
+        ></input>
+        <button onClick={() => this.addItem()}>Add TODO</button>
+        {this.state.noteList.map((note) => {
+          return <li key={note.id}>{note.value}</li>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Config/config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyDJ4Vv9iP33PwaeyMOsM9p-8OACZKrbxr8",
+  authDomain: "todolist-app-e19fb.firebaseapp.com",
+  databaseURL: "https://todolist-app-e19fb.firebaseio.com",
+  projectId: "todolist-app-e19fb",
+  storageBucket: "todolist-app-e19fb.appspot.com",
+  messagingSenderId: "701941799520",
+  appId: "1:701941799520:web:09668a3eda891bb4de0105",
+};

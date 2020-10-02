a76f3f8c86f8fbf18ac6521be56c4838d76c6cbf
eminvergil <emintrhack@gmail.com>
2020-10-02 12:51:10 +0300

    final change

--- src/App.js
+++ src/App.js
@@ -1,104 +1,116 @@
-import React from "react";
-import logo from "./logo.svg";
+import React from 'react';
 
-import { DB_CONFIG } from "./Config/config";
-import firebase from "firebase/app";
-import "firebase/database";
+import { DB_CONFIG } from './Config/config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-import "./App.css";
+import './App.css';
 
 var app = firebase.initializeApp(DB_CONFIG);
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.updateInput = this.updateInput.bind(this);
-    this.addItem = this.addItem.bind(this);
+		this.updateInput = this.updateInput.bind(this);
+		this.addItem = this.addItem.bind(this);
+		this.romeveItem = this.romeveItem.bind(this);
 
-    // this.app = firebase.initializeApp(DB_CONFIG);
-    this.database = app.database().ref().child("noteList");
+		// this.app = firebase.initializeApp(DB_CONFIG);
+		this.database = app.database().ref().child('noteList');
 
-    this.state = {
-      newNote: "",
-      noteList: [],
-    };
-  }
+		this.state = {
+			newNote: '',
+			noteList: []
+		};
+	}
 
-  updateInput(key, value) {
-    this.setState({
-      [key]: value,
-    });
-  }
+	updateInput(key, value) {
+		this.setState({
+			[key]: value
+		});
+	}
 
-  componentWillMount() {
-    const prevNotes = this.state.noteList;
+	componentWillMount() {
+		const prevNotes = this.state.noteList;
 
-    this.database.on("child_added", (snap) => {
-      prevNotes.push({
-        id: snap.key,
-        value: snap.val().noteList.value,
-      });
+		this.database.on('child_added', (snap) => {
+			prevNotes.push({
+				id: snap.key,
+				value: snap.val().noteList.value
+			});
 
-      this.setState({
-        noteList: prevNotes,
-      });
-    });
+			this.setState({
+				noteList: prevNotes
+			});
+		});
 
-    this.database.on("child_removed", (snap) => {
-      for (let i = 0; i < this.state.noteList.length; i++) {
-        if (prevNotes[i].id == snap.key) {
-          prevNotes.slice(i, 1);
-        }
-      }
+		console.log(prevNotes);
 
-      this.setState({
-        noteList: prevNotes,
-      });
-    });
-  }
+		// this.database.on('child_removed', (snap) => {
+		// 	for (let i = 0; i < prevNotes.length; i++) {
+		// 		if (prevNotes[i].id === snap.key) {
+		// 			prevNotes.slice(i, 1);
+		// 		}
+		// 	}
 
-  addItem() {
-    const newNote = {
-      id: 1 + Math.random(),
-      value: this.state.newNote.slice(),
-    };
-    // const noteList = [...this.state.noteList];
-    // noteList.push(newNote);
-    // this.setState({
-    //   newNote: "",
-    //   noteList,
-    // });
+		// 	this.setState({
+		// 		noteList: prevNotes
+		// 	});
+		// });
+	}
 
-    this.database.push().set({ noteList: newNote });
-  }
+	componentDidMount() {
+		const prevNotes = this.state.noteList;
+		this.database.on('child_removed', (snap) => {
+			for (let i = 0; i < prevNotes.length; i++) {
+				if (prevNotes[i].id === snap.key) {
+					prevNotes.splice(i, 1);
+				}
+			}
 
-  romeveItem(id) {
-    this.database.child(id).remove();
-  }
+			this.setState({
+				noteList: prevNotes
+			});
+		});
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Todo List App Made With Firebase</h1>
-        <input
-          type="text"
-          placeholder="Add your todo item"
-          value={this.state.newNote}
-          onChange={(e) => this.updateInput("newNote", e.target.value)}
-        ></input>
-        <button onClick={() => this.addItem()}>Add TODO</button>
-        {this.state.noteList.map((note) => {
-          return (
-            <li key={note.id}>
-              {note.value}{" "}
-              <button onClick={() => this.romeveItem(note.id)}>X</button>
-            </li>
-          );
-        })}
-      </div>
-    );
-  }
+	addItem() {
+		const newNote = {
+			id: 1 + Math.random(),
+			value: this.state.newNote.slice()
+		};
+
+		this.database.push().set({ noteList: newNote });
+	}
+
+	romeveItem(id) {
+		console.log('from removi item: ' + id);
+
+		this.database.child(id).remove();
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Todo List App Made With Firebase</h1>
+				<input
+					type="text"
+					placeholder="Add your todo item"
+					value={this.state.newNote}
+					onChange={(e) => this.updateInput('newNote', e.target.value)}
+				/>
+				<button onClick={() => this.addItem()}>Add TODO</button>
+				{this.state.noteList.map((note) => {
+					return (
+						<li key={note.id}>
+							{note.value} <button onClick={() => this.romeveItem(note.id)}>X</button>
+						</li>
+					);
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
